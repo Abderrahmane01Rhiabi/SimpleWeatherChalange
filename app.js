36d68c072a1deb262827cbac418299324ce3ad4f
Abderrahmane01Rhiabi <rhiabi.abderrahmane@gmail.com>
2023-02-06 17:19:55 +0100

    theerd commit

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const https = require("https");
 
-
+const  bodyParser = require("body-parser"); 
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended : true}));
+
+
 app.get("/", (req,res) =>{
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=paris&appid=1c077333c1658e5e8b96fd3fc7a62d2d&units=metric"
+    res.sendFile(__dirname+"/index.html")
+
+})
+
+app.post("/", (req,res) =>{
+
+    const query = req.body.city;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid=1c077333c1658e5e8b96fd3fc7a62d2d&units=metric"
+    
     https.get(url, (response)=>{
         console.log(response.statusCode);
 
@@ -25,7 +36,7 @@ app.get("/", (req,res) =>{
             
             //after write all this thing than send it res.send();
             
-            res.write("<h1>The temperature in London is "+temp+" degrees Celcius.</h1>");
+            res.write("<h1>The temperature in "+query+" is "+temp+" degrees Celcius.</h1>");
             res.write("<p>The Weather is currently "+wheatherData.weather[0].description+"</p>");
             res.write("<img src="+img+">");
             res.send();
@@ -34,9 +45,35 @@ app.get("/", (req,res) =>{
         }) 
 
     })
-
 })
 
+// const url = "https://api.openweathermap.org/data/2.5/weather?q=paris&appid=1c077333c1658e5e8b96fd3fc7a62d2d&units=metric"
+// https.get(url, (response)=>{
+//     console.log(response.statusCode);
+
+//     //get a data from the response and to parse this JSON data 
+//     response.on("data",(data)=>{
+//         const wheatherData = JSON.parse(data);//JSON.stringify(data) opposit
+//         const temp = wheatherData.main.temp; //accede a une attribut de lobjet
+//         const icon = wheatherData.weather[0].icon;
+//         const img = "https://openweathermap.org/img/wn/"+icon+"@2x.png"
+
+
+//         console.log(wheatherData); 
+//         console.log(icon); 
+//         console.log(temp); 
+        
+//         //after write all this thing than send it res.send();
+        
+//         res.write("<h1>The temperature in London is "+temp+" degrees Celcius.</h1>");
+//         res.write("<p>The Weather is currently "+wheatherData.weather[0].description+"</p>");
+//         res.write("<img src="+img+">");
+//         res.send();
+
+        
+//     }) 
+
+// })
 
 app.listen(3000, ()=>{
     console.log(("Server id running on port 3000"));
