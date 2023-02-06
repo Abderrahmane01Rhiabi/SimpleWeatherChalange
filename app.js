97a60f781ca015f61fc04aa6212ac7743101f08c
Abderrahmane01Rhiabi <rhiabi.abderrahmane@gmail.com>
2023-02-06 16:42:18 +0100

    second commit

--- app.js
+++ app.js
@@ -7,23 +7,34 @@ const app = express();
 
 app.get("/", (req,res) =>{
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=paris&appid=1c077333c1658e5e8b96fd3fc7a62d2d"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=paris&appid=1c077333c1658e5e8b96fd3fc7a62d2d&units=metric"
     https.get(url, (response)=>{
         console.log(response.statusCode);
 
         //get a data from the response and to parse this JSON data 
         response.on("data",(data)=>{
             const wheatherData = JSON.parse(data);//JSON.stringify(data) opposit
-            const temp = wheatherData.main.temp;
-            
-            console.log(temp); 
+            const temp = wheatherData.main.temp; //accede a une attribut de lobjet
+            const icon = wheatherData.weather[0].icon;
+            const img = "https://openweathermap.org/img/wn/"+icon+"@2x.png"
 
-        
+
+            console.log(wheatherData); 
+            console.log(icon); 
+            console.log(temp); 
+            
+            //after write all this thing than send it res.send();
+            
+            res.write("<h1>The temperature in London is "+temp+" degrees Celcius.</h1>");
+            res.write("<p>The Weather is currently "+wheatherData.weather[0].description+"</p>");
+            res.write("<img src="+img+">");
+            res.send();
+
+            
         }) 
 
     })
 
-    res.send("server is running");
 })
 
 
